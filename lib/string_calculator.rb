5785f44c95aafcfccbff3a3c37cd59a0971214f7
“Gisha <gishagireesh07@gmail.com>
2025-06-25 14:32:28 +0530

    Code: Handle custom dilimiter
    Signed-off-by: “Gisha <“gishagireesh07@gmail.com”>

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,11 @@ class StringCalculator
     0 if numbers.strip.empty?
 
     delimiter = /,|\n/
+    if numbers.start_with?("//")
+      custom_delimiter, numbers = numbers.split("\n")
+      delimiter = Regexp.escape(custom_delimiter[2])
+    end
+
     number_list = numbers.split(delimiter).map(&:to_i)
     number_list.sum
   end
