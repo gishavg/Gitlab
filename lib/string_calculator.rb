1dee901495ba0049b12756c0e1eac8c9c7d17b40
“Gisha <gishagireesh07@gmail.com>
2025-06-25 14:25:11 +0530

    Code: Handles any amount of numbers and newline delimiter
    Signed-off-by: “Gisha <“gishagireesh07@gmail.com”>

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,8 +2,8 @@ class StringCalculator
   def self.add(numbers)
     0 if numbers.strip.empty?
 
-    num = numbers.split(",").map(&:to_i)
-
-    num.sum
+    delimiter = /,|\n/
+    number_list = numbers.split(delimiter).map(&:to_i)
+    number_list.sum
   end
 end
